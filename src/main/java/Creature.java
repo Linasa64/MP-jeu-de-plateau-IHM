9f3e8795524b0f1b0043fcbb8d88b3ced1c12fbe
Hyris2012 <mariesara.mazen@yahoo.com>
2020-05-31 18:20:22 +0200

    nom créatures + modification system.out.println pas beau
    cartes ont un nom en fonction de leur force dans pioche et dans champs de bataille
    modif de "entrer numéro créature " pour attaquer

--- src/main/java/Creature.java
+++ src/main/java/Creature.java
@@ -3,18 +3,45 @@ public class Creature {
     int prix;
     int force;
     double reussite; // associé à la proba de réussite de la carte lors de l'attaque
+    String nomCreature; // pour faire plus accueillant
     
 
     public Creature (){
         this.prix=(int)(Math.random()*7)+1;
         this.force=this.prix;
         this.reussite=1-(this.force*0.1); // pour avoir une proba de réussite inversement proportionnelle à la force
-                
+          
+          if (this.force==1){
+			  this.nomCreature= "Gobelin serrurier";
+		  }
+		  else if  (this.force==2){
+			  this.nomCreature= "Elfe de l'agroforesterie";
+		  }
+		  else if (this.force==3){
+			  this.nomCreature= "Centaure busqué";
+		  }
+		  else if (this.force==4){
+			  this.nomCreature= "Griffon hirsute";
+		  }
+		  else if (this.force==5){
+			  this.nomCreature=  "Hydre emberlificotée";
+		  }
+		  else if (this.force==6){
+			  this.nomCreature= "Dragon tournebroche";
+		  }
+		  else {
+			  this.nomCreature= "licorne hypermétrope";
+		  }
     }
+    
 
     public int getPrix () {
         return this.prix;
     }
+    
+    public String getNom() {
+		return this.nomCreature;
+	}
 
     public int getForce () {
         return this.force;
