c4abf7717616cae8fca83bacb989b5c227e8d181
Hyris2012 <mariesara.mazen@yahoo.com>
2020-05-31 23:06:40 +0200

    déplacement du main vers partie
    déplacement du main vers partie avec la méthode lancerPartie()
    création constructeur Partie
    renommage de la méthode partie() en tourPartie() pour savoir à qui c'est de jouer

--- src/main/java/Magic.java
+++ src/main/java/Magic.java
@@ -1,12 +1,7 @@
 public class Magic {
     public static void main(String[] args){ 
-        Partie nouvellePartie=new Partie();
-        nouvellePartie.initialisation();
-
-        while (nouvellePartie.getJoueur1().getPV() >0 && nouvellePartie.getJoueur2().getPV()>0){
-            nouvellePartie.partie();
-        }
-
-        nouvellePartie.partieEstTerminee(nouvellePartie.getJoueur1(), nouvellePartie.getJoueur2());
+		
+		Partie nouvellePartie=new Partie();
+        
     }
 }
--- src/main/java/Partie.java
+++ src/main/java/Partie.java
@@ -3,6 +3,8 @@ public class Partie {
     private Joueur joueur2;
     private int numTour;
 
+
+
     public void partieEstTerminee (Joueur a, Joueur b) {
         if(a.getPV()<=0){
             System.out.println(a.getNomJoueur() + " a perdu, I hear CHEH in my oreillette");
@@ -12,7 +14,7 @@ public class Partie {
         }
     }
 
-    public void initialisation (){
+    public Partie (){
 
         this.joueur1=new Joueur(); 
         this.joueur2=new Joueur();
@@ -22,10 +24,12 @@ public class Partie {
 
         this.joueur1.genereMainDepart();
         this.joueur2.genereMainDepart();
+        
+        this.lancerPartie();
 
     }
 
-    public void partie () {
+    public void tourPartie () {
 
         if(this.numTour%2==0){
             tour(joueur1);
@@ -34,6 +38,16 @@ public class Partie {
             tour(joueur2);
         }
     }
+    
+    public void lancerPartie() { // méthode qui fait tourner la partie tant qu'aucun joueur n'a perdu
+		
+    
+        while (this.getJoueur1().getPV() >0 && this.getJoueur2().getPV()>0){
+            this.tourPartie();
+        }
+
+        this.partieEstTerminee(this.getJoueur1(), this.getJoueur2());
+	}
 
     public void tour (Joueur a){
 
